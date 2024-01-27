c341d4f5e8605fa2873ec71cbbe01fa79d00f629
YerimYeon <yinneu@gmail.com>
2024-01-27 14:50:14 +0000

    2024_01_27(3)

--- basic/b_2562.py
+++ basic/b_2562.py
@@ -1,2 +1,17 @@
+import sys
+
 nums = []
 
+maxV = 0
+maxI = 0
+
+for i in range(9):
+    nums.append(int(sys.stdin.readline()))
+
+for i in range(9):
+    if (nums[i] > maxV):
+        maxV = nums[i]
+        maxI = i+1
+
+print(maxV)
+print(maxI)
