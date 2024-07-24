ea74c9e953150b8b8b89966a8a812f4b1a6501ee
yerimYeon <99879845+yinneu@users.noreply.github.com>
2024-07-25 02:52:52 +0900

    240724

---
+++ basic/basic/b3_2460.py
@@ -0,0 +1,12 @@
+maxp = 0
+person = 0
+
+for _ in range(10):
+    a, b = map(int, input().split())
+    person = person - a + b
+    
+    if person > maxp:
+        maxp = person
+    
+print(maxp)
+    
