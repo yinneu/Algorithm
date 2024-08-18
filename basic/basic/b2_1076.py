2440be265f1bdb58aa6e10a7c41f704eb8c42620
YerimYeon <yinneu@gmail.com>
2024-08-19 01:24:29 +0900

    240819

---
+++ basic/basic/b2_1076.py
@@ -0,0 +1,19 @@
+om = {
+    'black': [0,1],
+    'brown': [1,10],
+    'red': [2,100],
+    'orange': [3,1000],
+    'yellow': [4,10000],
+    'green': [5,100000],
+    'blue': [6,1000000],
+    'violet': [7,10000000],
+    'grey': [8,100000000],
+    'white': [9,1000000000]
+}
+
+result = 0
+result += (om[input()][0] * 10)
+result += om[input()][0]
+result *= om[input()][1]
+
+print(result)
